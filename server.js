08ba314fb7f8ded2064247bc2ae89d37b2dda312
AlexanderCruz <SISTEMAS@NOTARIATAMBINI.COM>
2025-11-28 17:14:51 -0500

    add: last version

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3019;
 app.listen(PORT, () => {
-console.log(`✅ API corriendo en http://localhost:${PORT}`);
+console.log(`✅ API corriendo en http://192.168.1.20:${PORT}`);
 });
